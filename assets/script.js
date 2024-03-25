d8b39deb3ed655fe86c347380ea8f067c47783f2
Arabella Cueto <arabellampc@gmail.com>
2024-03-24 23:27:23 -0700

    uhhh test??

---
+++ assets/script.js
@@ -0,0 +1,13 @@
+var poop = 3;
+var eekiel = [1, 2, 3];
+console.log(eekiel.length)
+function pooooop(poop, eekiel) {
+    for (let index = 0; index < eekiel.length; index++) {
+        if (eekiel[index]==poop) {
+            console.log("weewooo")
+        }
+        
+    }    
+}
+
+pooooop(poop, eekiel)
